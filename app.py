69fabe49eb6e5cb105a52b087602342eb0189001
Cio <akiotryacharisma@gmail.com>
2026-04-28 11:22:52 +0700

    Add index.html homepage with nav bar

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from database.db import init_db
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'projectCatalyst is running!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     init_db()
